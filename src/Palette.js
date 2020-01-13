90f14c6fe8aa8ea5bd3b5fd74e352243d6324775
cdsalty <soltiscd@gmail.com>
2020-01-13 15:30:37 -0500

    added some styles and ColorBox component

--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import seedPalettes from './seedPalettes';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Palette">
         {/* Navbar to go here */}
-        <div className="Palette-colors">{/* will display a bunch of color boxes */}</div>
+        <div className="Palette-colors">
+          {/* will display a bunch of color boxes */}
+          {colorBoxes}
+        </div>
         {/* Footer to go here */}
       </div>
     );
