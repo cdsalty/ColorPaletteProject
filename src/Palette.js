4be8e2096918bf3fe107630c639c6410c1e9a10f
cdsalty <soltiscd@gmail.com>
2020-01-20 23:26:25 -0500

    updated syling; navbar next

--- src/Palette.js
+++ src/Palette.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = {level: 500};
     this.changeLevel = this.changeLevel.bind(this);
   }
   changeLevel(level) {
-    this.setState({ level });
+    this.setState({level});
   }
   render() {
-    const { colors } = this.props.palette;
-    const { level } = this.state;
+    const {colors} = this.props.palette;
+    const {level} = this.state;
     const colorBoxes = colors[level].map((color) => (
       <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         {/* Navbar to go here */}
         <div className="Palette-colors">
           {/* will display a bunch of color boxes */}
